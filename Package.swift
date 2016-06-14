0af67d2a924889b70e37908a9f756f8ce3643c3a
Bouke Haarsma <bouke@haarsma.eu>
2016-06-14 21:14:28 +0200

    Use fixed CSV module

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "LedgerTools",
     dependencies: [
-        .Package(url: "https://github.com/Bouke/CSV.git", versions: Version(1, 0, 5)..<Version(2, 0, 0)),
+        .Package(url: "https://github.com/Bouke/CSV.git", versions: Version(1, 0, 6)..<Version(2, 0, 0)),
         .Package(url: "https://github.com/Bouke/Glob.git", versions: Version(1, 0, 1)..<Version(2, 0, 0)),
     ],
     targets: [
